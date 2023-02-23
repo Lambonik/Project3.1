f82b1c41ea5b9ff8ba303226a647119d659b1da9
Lambonik <nikita1994g@mail.ru>
2023-02-23 19:56:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int value = 20;
+        int bonus;
+        
+        bonus = price / value;
+
+        System.out.println(bonus);
+    }
+}
